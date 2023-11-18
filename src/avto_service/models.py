7a3286c6e77d1160c45eaa2d4a1f14319a316e2e
Not-user-1984 <pluzhnikov.d@team.nekidaem.ru>
2023-11-18 09:38:13 +0300

    add models.py

--- src/avto_service/models.py
+++ src/avto_service/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Mark(models.Model):
+    """
+    Модель для хранения информации о марках автомобилей.
+    """
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Model(models.Model):
+    """
+    Модель для хранения информации о моделях автомобилей.
+    """
+    mark = models.ForeignKey(Mark, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    generation = models.IntegerField()
+    modification_name = models.CharField(max_length=100)
+    modification_id = models.IntegerField()
+    configuration_id = models.IntegerField()
+    tech_param_id = models.IntegerField()
+    body_type = models.CharField(max_length=100)
+    years = models.CharField(max_length=100)
+    complectations = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.mark.name} - {self.name} ({self.modification_name})"
