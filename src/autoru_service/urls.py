b4d3639aebabcf29a9c571b9d52b57507f2613fe
Not-user-1984 <pluzhnikov.d@team.nekidaem.ru>
2023-11-19 12:31:05 +0300

    add final

--- src/autoru_service/urls.py
+++ src/autoru_service/urls.py
@@ -3,5 +3,6 @@ from autoru_service import views
 
 urlpatterns = [
     path('update_autoru_catalog/', views.update_autoru_catalog, name='update_autoru_catalog'),
+    path('delete_autoru_catalog/', views.delete_autoru_catalog, name='delete_autoru_catalog'),
     path('', views.home, name='home'),
 ]
--- src/autoru_service/views.py
+++ src/autoru_service/views.py
@@ -1,30 +1,53 @@
-from datetime import datetime, timedelta
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from autoru_service.models import Mark, Model
 from autoru_service.perser_avto_to_db import parse_xml_file
 
 
 def update_autoru_catalog(request):
-    last_update = Model.objects.order_by('-id').first()
-    if not last_update:
-        Mark.objects.all().delete()
-        Model.objects.all().delete()
-        parse_xml_file()
-        return redirect('home')
-    else:
-        Mark.objects.all().delete()
-        Model.objects.all().delete()
-        parse_xml_file()
-        return redirect('home')
+    Mark.objects.all().delete()
+    Model.objects.all().delete()
+    parse_xml_file()
+    return redirect('home')
+
+
+def delete_autoru_catalog(request):
+    Mark.objects.all().delete()
+    Model.objects.all().delete()
+    return redirect('home')
+
+
+def paginate_models(request, models):
+    paginator = Paginator(models, 10)
+    page_number = request.GET.get('page')
+    try:
+        paginated_models = paginator.page(page_number)
+    except PageNotAnInteger:
+        paginated_models = paginator.page(1)
+    except EmptyPage:
+        paginated_models = paginator.page(paginator.num_pages)
+    return paginated_models
+
+
 
 def home(request):
     all_marks = Mark.objects.all()
-    if request.method == 'GET' and not all_marks:
+    selected_mark = request.GET.get('selected_mark')
+
+    if not all_marks:
         return render(request, 'home.html', {'no_data': True})
-    elif request.method == 'POST':
+
+    if request.method == 'POST':
         selected_mark = request.POST.get('selected_mark')
+        return HttpResponseRedirect(f"{reverse('home')}?selected_mark={selected_mark}")
+
+    if request.GET.get('selected_mark'):
         models_of_selected_mark = Model.objects.filter(mark__name=selected_mark)
-        return render(request, 'home.html', {'marks': all_marks, 'models': models_of_selected_mark})
+        paginated_models = paginate_models(request, models_of_selected_mark)
+        return render(request, 'home.html',
+                      {'marks': all_marks, 'models': paginated_models, 'selected_mark': selected_mark})
+
     return render(request, 'home.html', {'marks': all_marks})
